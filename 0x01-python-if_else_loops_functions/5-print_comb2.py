51bf68f8301c63042847e32a2529c6c3c3b5fc54
Webprotekh <webprotekh@gmail.com>
2021-11-21 02:46:28 -0800

    updated

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 for nums in renge(0, 100):
-    print("{:02d}".format(nums), end="")
-    if nums == 99:
-        break
-    print(", ", end="")
+    print("{:02d}".format(nums), end="\n" if nums == 99 else ", ")
