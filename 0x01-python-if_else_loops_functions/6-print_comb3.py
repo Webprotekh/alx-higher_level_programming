a2ed1a5a6bdcae4f82e0a6229fb11026d2ca2833
Webprotekh <webprotekh@gmail.com>
2021-11-25 02:52:39 -0800

    updated

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+for nums in range(0, 100):
+    for nums2 in range(0, 10):
+        if nums == nums2:
+            continue
+        elif nums > nums2:
+            continue
+        print("{:d}{:d}".format(nums, nums2), end="")
+        if ((nums == 8) and (nums2 == 9)):
+            break
+        print(", ", end = "")
