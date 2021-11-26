05e7534f8a1997e70f7fc1419cefda72b83c45cb
Webprotekh <webprotekh@gmail.com>
2021-11-26 12:48:10 -0800

    updated

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-for nums in range(0, 100):
-    for nums2 in range(0, 10):
-        if nums == nums2:
-            continue
-        elif nums > nums2:
-            continue
-        print("{:d}{:d}".format(nums, nums2), end="")
-        if ((nums == 8) and (nums2 == 9)):
-            break
-        print(", ", end="")
+number = 0
+while number <= 89:
+    if number % 10 == 0:
+        number += 1 + number // 10
+    print("{:02d}".format(number), end='\n' if number == 89 else ", ")
+    number += 1
