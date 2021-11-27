5206e24efca2707fb631c00e2f059605feaa3011
Webprotekh <webprotekh@gmail.com>
2021-11-27 11:38:16 -0800

    updated

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 for n in reversed(range(97, 123)):
-    print("{}".format(chr(n)) if (n % 2 == 0) else "{}".format(chr(n - 32)), end='')
+    print("{}".format(chr(n)) if (n % 2 == 0) else "{}".format(chr(n - 32)),\
+            end='')
