e9c05bf31bed970d59e6598d4fa22856556af96c
Webprotekh <webprotekh@gmail.com>
2021-12-01 06:45:33 -0800

    updated

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,17 +4,18 @@ import sys
 
 def main(*argv):
     i = 0
-    l = len(sys.argv) - 1
-    if l == 1:
-        print("{:d} argument:".format(l))
-    elif l == 0:
-        print("{:d} arguments.".format(l))
+    count = len(sys.argv) - 1
+    if count == 1:
+        print("{:d} argument:".format(count))
+    elif count == 0:
+        print("{:d} arguments.".format(count))
     else:
-        print("{:d} arguments:".format(l))
+        print("{:d} arguments:".format(count))
     for args in sys.argv:
         if (i != 0):
             print("{}: {}".format(i, args))
         i += 1
+
 
 if __name__ == "__main__":
     main()
