a87110124e683523bea34b923360a3de14f7b5d4
Webprotekh <webprotekh@gmail.com>
2021-12-18 05:52:08 -0800

    updated

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for i in my_string:
-        if (i == chr(99) or i == chr(67)):
-            continue
-        print("{}".format(i), end=" ")
-    print()
+    my_string = my_string.translate({ord('c'): None})
+    my_string = my_string.translate({ord('C'): None})
+    return my_string
